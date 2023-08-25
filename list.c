311b20fc13e0000c664f9255d1385971380f1420
Replit user <>
2023-08-25 05:31:55 +0000

    new try

--- list.c
+++ list.c
@@ -49,11 +49,9 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
-  if (list == NULL || list->tail == NULL) {
-    return NULL; // Lista vacía 
-  }
+  if (list == NULL || list->tail == NULL)return NULL;
   list->current = list->tail;
-  return &(list->tail->data); // Retornar ultimo dato
+  return (list->tail->data); // Retornar ultimo dato
 }
 
 void * prevList(List * list) {
